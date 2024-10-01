80a16454f4fba4c7c3f6d287059120dcc06b5b46
RyanOOSD <ryan.angaangan@edu.sait.ca>
2024-10-01 00:37:24 -0600

    Finished form confirmation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-// Adding an event handler to the "Register" button
+// Confirming submit on the form
+// Uses a "click" event listener on the submit-btn ID
 document.querySelector("#submit-btn")
-    .addEventListener
+    .addEventListener("click", function(r) {
+        // Prevents the page from being refreshed
+        r.preventDefault();
+        if (confirm("Would you like to submit the form?")) {
+            document.querySelector("form").submit();
+        }
+        else {
+            alert("Submit cancelled.")
+        }
+});
+
+// Confirming reset on the form
+// Uses a "click" event listener on the reset-btn ID
+document.querySelector("#reset-btn")
+    .addEventListener("click", function(r) {
+        r.preventDefault();
+        if (confirm("Would you like to reset the form?")) {
+            document.querySelector("form").reset();
+        } else {
+            alert("Reset cancelled.");
+        }
+});
+
+// Display desciptions for each field in the form
